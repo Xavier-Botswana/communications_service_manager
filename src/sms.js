96ce82f217f376cf27f50b69e72ed90aad5a47ca
rPrinceA495 <princepappiah@gmail.com>
2020-08-12 15:08:53 +0200

    firebase functions to send sms

--- src/sms.js
+++ src/sms.js
@@ -3,10 +3,13 @@ const axios = require("axios");
 const sendSMS = (to, body) => {
   to = `+267${to}`;
   axios
-    .post("/sms", {
-      to: to,
-      body: body,
-    })
+    .post(
+      "https://us-central1-ag-nutrition-hctrhq.cloudfunctions.net/app/sms",
+      {
+        to: to,
+        body: body,
+      }
+    )
     .catch((error) => {
       console.log(error);
     });
