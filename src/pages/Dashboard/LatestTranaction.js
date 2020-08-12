ea72ce15d64249dc3f38276851e0f4ef2fcbc642
rPrinceA495 <princepappiah@gmail.com>
2020-08-12 02:47:38 +0200

    Log added to dashboard

--- src/pages/Dashboard/LatestTranaction.js
+++ src/pages/Dashboard/LatestTranaction.js
@@ -1,226 +1,70 @@
-import React from "react";
-import { Card, CardBody, CardTitle, Badge, Button } from "reactstrap";
+import React, { useState, useEffect } from "react";
+import { MDBDataTable } from "mdbreact";
+import { Row, Col, Card, CardBody, CardTitle, Badge, Button } from "reactstrap";
 import { Link } from "react-router-dom";
 
+import firebase from "../../firebase";
+
 const LatestTranaction = (props) => {
-  const transactions = [
-    {
-      id: "customCheck2",
-      orderId: "#1",
-      billingName: "mk2300days@hotmail.com",
-      Date: "01 Aug, 2020",
-      total: "$400",
-      badgeClass: "success",
-      paymentStatus: "Paid",
-      methodIcon: "",
-      paymentMethod: "Super Admin",
-      link: "#",
-    },
-    {
-      id: "customCheck2",
-      orderId: "#2",
-      billingName: "amamahupe@gmail.com ",
-      Date: "01 Aug, 2020",
-      total: "$400",
-      badgeClass: "success",
-      paymentStatus: "Paid",
-      methodIcon: "",
-      paymentMethod: "Super Admin",
-      link: "#",
-    },
-    {
-      id: "customCheck2",
-      orderId: "#3",
-      billingName: "ketlhoilwe17@homail.com",
-      Date: "01 Aug, 2020",
-      total: "$400",
-      badgeClass: "success",
-      paymentStatus: "Paid",
-      methodIcon: "",
-      paymentMethod: "Super Admin",
-      link: "#",
-    },
-    {
-      id: "customCheck2",
-      orderId: "#4",
-      billingName: "ksmazongo@gmail.com",
-      Date: "01 Aug, 2020",
-      total: "$400",
-      badgeClass: "success",
-      paymentStatus: "Paid",
-      methodIcon: "",
-      paymentMethod: "Admin",
-      link: "#",
-    },
-    {
-      id: "customCheck2",
-      orderId: "#5",
-      billingName: "marymocu@gmail.com ",
-      Date: "01 Aug, 2020",
-      total: "$400",
-      badgeClass: "success",
-      paymentStatus: "Paid",
-      methodIcon: "",
-      paymentMethod: "Admin",
-      link: "#",
-    },
-    {
-      id: "customCheck2",
-      orderId: "#6",
-      billingName: "keabaitset@gmail.com",
-      Date: "01 Aug, 2020",
-      total: "$400",
-      badgeClass: "success",
-      paymentStatus: "Paid",
-      methodIcon: "",
-      paymentMethod: "Admin",
-      link: "#",
-    },
-    {
-      id: "customCheck2",
-      orderId: "#7",
-      billingName: "oaitse@gmail.com",
-      Date: "01 Aug, 2020",
-      total: "$400",
-      badgeClass: "success",
-      paymentStatus: "Paid",
-      methodIcon: "",
-      paymentMethod: "Admin",
-      link: "#",
-    },
-    {
-      id: "customCheck2",
-      orderId: "#8",
-      billingName: "keabaitset@gmail.com",
-      Date: "01 Aug, 2020",
-      total: "$400",
-      badgeClass: "success",
-      paymentStatus: "Paid",
-      methodIcon: "",
-      paymentMethod: "Admin",
-      link: "#",
-    },
-    {
-      id: "customCheck2",
-      orderId: "#9",
-      billingName: "Marciadinake@gmail.com",
-      Date: "01 Aug, 2020",
-      total: "$400",
-      badgeClass: "success",
-      paymentStatus: "Paid",
-      methodIcon: "",
-      paymentMethod: "Finance",
-      link: "#",
-    },
-    {
-      id: "customCheck2",
-      orderId: "#10",
-      billingName: "mogajanebosa@gmail.com",
-      Date: "01 Aug, 2020",
-      total: "$400",
-      badgeClass: "success",
-      paymentStatus: "Paid",
-      methodIcon: "",
-      paymentMethod: "Finance",
-      link: "#",
-    },
-    {
-      id: "customCheck2",
-      orderId: "#11",
-      billingName: "ogaufingwanasejo@gmail.com",
-      Date: "01 Aug, 2020",
-      total: "$400",
-      badgeClass: "success",
-      paymentStatus: "Paid",
-      methodIcon: "",
-      paymentMethod: "Finance",
-      link: "#",
-    },
-    {
-      id: "customCheck2",
-      orderId: "#12",
-      billingName: "ratigalekgorwe@gmail.com",
-      Date: "01 Aug, 2020",
-      total: "$400",
-      badgeClass: "success",
-      paymentStatus: "Paid",
-      methodIcon: "",
-      paymentMethod: "Finance",
-      link: "#",
-    },
-  ];
+  const [logs, setLogs] = useState([]);
+
+  useEffect(() => {
+    const db = firebase.db.collection("activity_log");
+
+    db.onSnapshot((snapshot) => {
+      const retrievedlogs = [];
+
+      snapshot.forEach((doc) => {
+        retrievedlogs.push({ ...doc.data(), id: doc.id });
+      });
+
+      setLogs(retrievedlogs);
+    });
+  }, []);
+
+  const data = {
+    columns: [
+      {
+        label: "id",
+        field: "id",
+        sort: "asc",
+        width: 100,
+      },
+      {
+        label: "user",
+        field: "user",
+        sort: "asc",
+        width: 100,
+      },
+      {
+        label: "action",
+        field: "action",
+        sort: "asc",
+        width: 100,
+      },
+      {
+        label: "time",
+        field: "time",
+        sort: "asc",
+        width: 100,
+      },
+    ],
+
+    rows: logs,
+  };
 
   return (
-    <React.Fragment>
-      <Card>
-        <CardBody>
-          <CardTitle className="mb-4">Application Users</CardTitle>
-          <div className="table-responsive">
-            <table className="table table-centered table-nowrap mb-0">
-              <thead className="thead-light">
-                <tr>
-                  <th style={{ width: "20px" }}>
-                    <div className="custom-control custom-checkbox">
-                      <input
-                        type="checkbox"
-                        className="custom-control-input"
-                        id="customCheck1"
-                      />
-                      <label
-                        className="custom-control-label"
-                        htmlFor="customCheck1"
-                      >
-                        &nbsp;
-                      </label>
-                    </div>
-                  </th>
-                  <th>User ID</th>
-                  <th>Email</th>
-                  <th>Date Added</th>
-                  <th>User Type</th>
-                </tr>
-              </thead>
-              <tbody>
-                {transactions.map((transaction, key) => (
-                  <tr key={"_tr_" + key}>
-                    <td>
-                      <div className="custom-control custom-checkbox">
-                        <input
-                          type="checkbox"
-                          className="custom-control-input"
-                          id={transaction.id}
-                        />
-                        <label
-                          className="custom-control-label"
-                          htmlFor={transaction.id}
-                        >
-                          &nbsp;
-                        </label>
-                      </div>
-                    </td>
-                    <td>
-                      <Link to="#" className="text-body font-weight-bold">
-                        {" "}
-                        {transaction.orderId}{" "}
-                      </Link>{" "}
-                    </td>
-                    <td>{transaction.billingName}</td>
-                    <td>{transaction.Date}</td>
+    <Row>
+      <Col className="col-12">
+        <Card>
+          <CardBody>
+            <CardTitle>System Log Of All User Changes </CardTitle>
 
-                    <td>
-                      <i
-                        className={"fab " + transaction.methodIcon + " mr-1"}
-                      ></i>{" "}
-                      {transaction.paymentMethod}
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </CardBody>
-      </Card>
-    </React.Fragment>
+            <MDBDataTable responsive bordered data={data} exportToCSV />
+          </CardBody>
+        </Card>
+      </Col>
+    </Row>
   );
 };
 
--- src/pages/Projects/systemlog.js
+++ src/pages/Projects/systemlog.js
@@ -1,98 +1,82 @@
-import React, { useState, useCallback, useContext, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { MDBDataTable } from "mdbreact";
 import { Row, Col, Card, CardBody, CardTitle, CardSubtitle } from "reactstrap";
 
 //Import Breadcrumb
-import Breadcrumbs from '../../components/Common/declinedwithdrawals';
+import Breadcrumbs from "../../components/Common/declinedwithdrawals";
 import "./datatables.scss";
 import Layout from "../../components/HorizontalLayout";
-import  firebase from '../../firebase';
-
+import firebase from "../../firebase";
 
 const SystemLogs = (props) => {
+  const [logs, setLogs] = useState([]);
 
+  useEffect(() => {
+    const db = firebase.db.collection("activity_log");
 
-  const [ logs, setLogs ] = useState([])
+    db.onSnapshot((snapshot) => {
+      const retrievedlogs = [];
 
-  useEffect( () => {
-      const db = firebase.db.collection('activity_log')
-  
-      db.onSnapshot( snapshot => {
-          const retrievedlogs = []
-  
-          snapshot.forEach( doc => {
-              retrievedlogs.push({...doc.data(), id: doc.id})
-          })
-  
-          setLogs(retrievedlogs)
-      })
-  }, [])
- 
+      snapshot.forEach((doc) => {
+        retrievedlogs.push({ ...doc.data(), id: doc.id });
+      });
 
- 
- 
-  
-   const data = {
-      columns: [
-        {
-          label: "id",
-          field: "id",
-          sort: "asc",
-          width: 100
-        },
-        {
-          label: "user",
-          field: "user",
-          sort: "asc",
-          width: 100
-        },
-        {
-          label: "action",
-          field: "action",
-          sort: "asc",
-          width: 100
-        },
-        {
-          label: "time",
-          field: "time",
-          sort: "asc",
-          width: 100
-        }
-       
+      setLogs(retrievedlogs);
+    });
+  }, []);
 
-      ],
-      
-      rows: logs
-        };
+  const data = {
+    columns: [
+      {
+        label: "id",
+        field: "id",
+        sort: "asc",
+        width: 100,
+      },
+      {
+        label: "user",
+        field: "user",
+        sort: "asc",
+        width: 100,
+      },
+      {
+        label: "action",
+        field: "action",
+        sort: "asc",
+        width: 100,
+      },
+      {
+        label: "time",
+        field: "time",
+        sort: "asc",
+        width: 100,
+      },
+    ],
 
+    rows: logs,
+  };
 
-    return (
-      <Layout>
-        <div className="page-content">
-          <div className="container-fluid">
+  return (
+    <Layout>
+      <div className="page-content">
+        <div className="container-fluid">
+          <Breadcrumbs title="System User Logs" />
 
-            <Breadcrumbs title="System User Logs" />
+          <Row>
+            <Col className="col-12">
+              <Card>
+                <CardBody>
+                  <CardTitle>System Log Of All User Changes </CardTitle>
 
-            <Row>
-              <Col className="col-12">
-                <Card>
-                  <CardBody>
-                    <CardTitle>System Log Of All User Changes </CardTitle>
-                   
-
-                    <MDBDataTable responsive bordered data={data} exportToCSV />
-
-
-                  </CardBody>
-                </Card>
-              </Col>
-            </Row>
-
-          
-          </div>
+                  <MDBDataTable responsive bordered data={data} exportToCSV />
+                </CardBody>
+              </Card>
+            </Col>
+          </Row>
         </div>
-        </Layout>
-      );
-    };
+      </div>
+    </Layout>
+  );
+};
 
 export default SystemLogs;
