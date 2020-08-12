d5b37adab25a8caee43688f5dcd365273f275e00
rPrinceA495 <princepappiah@gmail.com>
2020-08-12 13:36:17 +0200

    firebase functions sms

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,63 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
 const express = require("express");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express();
+const bodyParser = require("body-parser");
+const https = require("https");
+
+const allowCrossDomain = function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+};
+
+app.use(allowCrossDomain);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/test", (req, res) => res.send("Backend reached."));
+
+app.post("/sms", (req, res) => {
+  let username = "xavier_africa";
+  let password = "@Xav!er123";
+
+  let postData = JSON.stringify({
+    to: req.body.to,
+    body: req.body.body,
+  });
+
+  let options = {
+    hostname: "api.bulksms.com",
+    port: 443,
+    path: "/v1/messages",
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Content-Length": postData.length,
+      Authorization:
+        "Basic " + Buffer.from(username + ":" + password).toString("base64"),
+    },
+  };
+
+  req = https.request(options, (resp) => {
+    console.log("statusCode:", resp.statusCode);
+    let data = "";
+    resp.on("data", (chunk) => {
+      data += chunk;
+    });
+    resp.on("end", () => {
+      console.log("Response:", data);
+    });
+  });
+
+  req.on("error", (e) => {
+    console.error(e);
+  });
+
+  req.write(postData);
+  req.end();
+});
+
+exports.app = functions.https.onRequest(app);
--- src/sms.js
+++ src/sms.js
@@ -3,7 +3,10 @@ const axios = require("axios");
 const sendSMS = (to, body) => {
   to = `+267${to}`;
   axios
-    .post("http://localhost:5000/sms", { to: to, body: body })
+    .post("/sms", {
+      to: to,
+      body: body,
+    })
     .catch((error) => {
       console.log(error);
     });
